6dd4d7f46fe175b6fe5a27c2a378010cd1854985
YarikGrabovets <93872617+YarikGrabovets@users.noreply.github.com>
2021-11-07 20:07:44 +0200

    Update foodClass.py

--- foodClass.py
+++ foodClass.py
@@ -1,10 +1,11 @@
 import pygame, os
 from mainConst import action, screen, pixel_font
+from abs_path import abs_path
 
 pygame.init()
 
 clicked_feed = False
-coin_sound = pygame.mixer.Sound('sounds/coin.mp3')
+coin_sound = pygame.mixer.Sound(abs_path('sounds/coin.ogg'))
 
 
 class FoodMenu:
@@ -34,7 +35,7 @@ class FoodMenu:
         self.food_2_rect = self.food_2.get_rect(center=(self.x, self.y - 10))
         self.food_3_rect = self.food_3.get_rect(center=(self.x + 250, self.y))
 
-        self.exit = pygame.transform.scale(pygame.image.load('images/sprites/iconCross_beige.png'), (40, 40))
+        self.exit = pygame.transform.scale(pygame.image.load(abs_path('images/sprites/iconCross_beige.png')), (40, 40))
         self.exit_rect = self.exit.get_rect(center=(75, 65))
 
     def blit_food_menu(self):
@@ -55,19 +56,19 @@ class FoodMenu:
         if self.box_1_rect.collidepoint((x, y)):
             text = pixel_font.render('Prise: 5 Lg Effects: +3', True, (255, 255, 255))
             screen.blit(text, (20, 120))
-            self.box_1 = pygame.transform.scale(pygame.image.load('images/sprites/buttonSquare_blue.png'), (150, 150))
+            self.box_1 = pygame.transform.scale(pygame.image.load(abs_path('images/sprites/buttonSquare_blue.png')), (150, 150))
         else:
             self.box_1 = self.normal_image
         if self.box_2_rect.collidepoint((x, y)):
             text = pixel_font.render('Prise: 10 Lg Effects: +6', True, (255, 255, 255))
             screen.blit(text, (270, 120))
-            self.box_2 = pygame.transform.scale(pygame.image.load('images/sprites/buttonSquare_blue.png'), (150, 150))
+            self.box_2 = pygame.transform.scale(pygame.image.load(abs_path('images/sprites/buttonSquare_blue.png')), (150, 150))
         else:
             self.box_2 = self.normal_image
         if self.box_3_rect.collidepoint((x, y)):
             text = pixel_font.render('Prise: 15 Lg Effects: +10', True, (255, 255, 255))
             screen.blit(text, (480, 120))
-            self.box_3 = pygame.transform.scale(pygame.image.load('images/sprites/buttonSquare_blue.png'), (150, 150))
+            self.box_3 = pygame.transform.scale(pygame.image.load(abs_path('images/sprites/buttonSquare_blue.png')), (150, 150))
         else:
             self.box_3 = self.normal_image
 
