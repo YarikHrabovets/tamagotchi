d79b2e8b50696f596a60f356a49fecf0e6ea44ac
YarikGrabovets <93872617+YarikGrabovets@users.noreply.github.com>
2021-11-07 20:24:09 +0200

    Update mainConst.py

--- mainConst.py
+++ mainConst.py
@@ -1,4 +1,5 @@
 import pygame, os
+from abs_path import abs_path
 
 pygame.init()
 
@@ -6,12 +7,12 @@ width = 150
 height = 150
 action = {'satiety': 100, 'toilet': 100, 'happy': 100, 'health': 100, 'logiki': 50}
 screen = pygame.display.set_mode((800, 500))
-pixel_font = pygame.font.Font('font/technicality1.ttf', 27)
-tamagotchiJump = [pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-0.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-1.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-2.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-3.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-4.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-5.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-6.png'), (width, height)),
-                  pygame.transform.scale(pygame.image.load('images/sprites/tamagotchi animation/tamagotchi-7.png'), (width, height))]
+pixel_font = pygame.font.Font(abs_path('font/technicality1.ttf'), 27)
+tamagotchiJump = [pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-0.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-1.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-2.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-3.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-4.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-5.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-6.png')), (width, height)),
+                  pygame.transform.scale(pygame.image.load(abs_path('images/sprites/tamagotchi animation/tamagotchi-7.png')), (width, height))]
